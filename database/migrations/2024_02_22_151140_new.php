af848af7f28df1336f6ced7be943307c12a3bb73
BONGUILI Archange <bonguiliarchange8@gmail.com>
2024-02-26 22:37:35 +0100

    Nouvelle migration

---
+++ database/migrations/2024_02_22_151140_new.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Galeries;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::table('vehicules', function(Blueprint $table){
+            $table->foreignIdFor(Galeries::class)->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+
+        Schema::table('vehicules', function(Blueprint $table){
+            $table->dropForeignIdFor(Galeries::class);
+        });
+    }
+};
